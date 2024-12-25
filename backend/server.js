03f3e102eb7d372669c96a3de8bbb24e63797ad7
dunalpasindu <inbox.dunal@gmail.com>
2024-12-25 14:36:01 +0530

    routes testing

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ require('dotenv').config();
 const connectDB = require('./config'); //import db connection fun. from config.js
 const logger = require('./utils/logger'); //import custom logger
 
+const roomsRoute = require('./routes/roomsRoute');
+
 const app = express();
 const port = process.env.PORT || 5005;
-
 
 connectDB(); //call db connection fun. from config.js
 
@@ -14,10 +15,13 @@ connectDB(); //call db connection fun. from config.js
 app.use(cors());
 app.use(express.json());
 
+
 // Routes
 app.get('/', (req, res) => {
   res.send('<h2>Hello! Your Backend is UP..</h2>');
 });
+app.use('/rooms', roomsRoute);
+
 
 // Start server
 app.listen(port, () => {
